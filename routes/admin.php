6b20ea167da94bdeb22164a03c43950867c25b7f
seddar <ephmaghni@gmail.com>
2023-07-08 21:26:59 +0200

    login page

--- routes/admin.php
+++ routes/admin.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/admin', function () {
-    return view('layouts.admin');
+Route::get('/admin/login', function () {
+    return view('admin.auth.login');
 });
