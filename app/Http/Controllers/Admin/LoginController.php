8a99169d56a209cebd54184c63a3ee3c92c36e9e
seddar <ephmaghni@gmail.com>
2023-07-10 18:41:43 +0200

    logout

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -9,28 +9,33 @@ use App\Http\Requests\LoginRequest;
 
 class LoginController extends Controller
 {
-    public function show_login_view(){
-return view ('admin.auth.login');
-/*
-        $admin['name']="seddar mohammed";
-        $admin['email']="seddar@gmail.com";
-        $admin['username']="seddar";
-        $admin['password']=bcrypt("admin");
-        $admin['active'] = "1";
-        $admin['date'] = date("Y-m-d");
-        $admin['com_code'] = 1;
-        $admin['added_by'] = 1;
-        $admin['updated_by'] = 1;
-        Admin::create($admin);
-*/
+    public function show_login_view()
+    {
+        return view('admin.auth.login');
+        /*
+                $admin['name']="seddar mohammed";
+                $admin['email']="seddar@gmail.com";
+                $admin['username']="seddar";
+                $admin['password']=bcrypt("admin");
+                $admin['active'] = "1";
+                $admin['date'] = date("Y-m-d");
+                $admin['com_code'] = 1;
+                $admin['added_by'] = 1;
+                $admin['updated_by'] = 1;
+                Admin::create($admin);
+        */
     }
 
     public function login(LoginRequest $request)
     {
         if (auth()->guard('admin')->attempt(['username' => $request->input('username'), 'password' => $request->input('password')])) {
-            return view('layouts.admin');
+            return redirect()->route('admin.dashboard');
         } else {
             return redirect()->route('admin.showlogin')->with(['error' => 'عفوا بيانات التسجيل غير صحيحة']);
         }
-}
+    }
+    public function logout(){
+        auth()->logout();
+    return redirect()->route('admin.showlogin');
+    }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::group(['namespace'=>'Admin','prefix'=>'admin','middleware'=>'guest'],function(){
+Route::group(['namespace'=>'Admin','prefix'=>'admin','middleware'=>'auth:admin'],function(){
+Route::get('/',[DashboardController::class,'index'])->name('admin.dashboard');
+Route::get('/logout', [LoginController::class,'logout'])->name('admin.logout');
+});
+
+Route::group(['namespace'=>'Admin','prefix'=>'admin','middleware'=>'guest:admin'],function(){
     Route::get('login',[LoginController::class,'show_login_view'])->name('admin.showlogin');
     Route::post('login', [LoginController::class, 'login'])->name('admin.login');
 });
